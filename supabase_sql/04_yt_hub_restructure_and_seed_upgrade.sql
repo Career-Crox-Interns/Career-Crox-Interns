18e4498620e7e93d3331e377d2340345bfceb95d
Career-Crox-Interns <hrteam.careercrox@gmail.com>
2026-04-08 12:15:34 +0530

    Delete supabase_sql directory

--- supabase_sql/04_yt_hub_restructure_and_seed_upgrade.sql
+++
@@ -1,3 +0,0 @@
--- Career Crox Freelancer CRM - no-op file
--- YT Hub / sample playlist seeding has been removed from this live freelancer pack.
-select 'YT Hub seed removed for live freelancer CRM' as status;
